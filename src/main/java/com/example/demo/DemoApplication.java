2eb3fe9bb84125877dda31f78fc680a78643695b
cjsonnnnn <cjsonnnnn@gmail.com>
2025-07-21 09:06:41 +0700

    [DEV] added debug and changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "Hello, Jason! The pipeline works as expected!";
+    return "Hello, Jason! The pipeline works as expected! 0.0.7";
   }
 }
