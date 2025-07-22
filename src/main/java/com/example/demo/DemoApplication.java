8ac9b1591c834733fcf3b9a5213d7f82b819cb11
cjsonnnnn <cjsonnnnn@gmail.com>
2025-07-22 11:56:22 +0700

    [DEV] 6th

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "Hello, Jason! The pipeline works as expected! Changes: this content!";
+    return "Hello, Jason! The pipeline works as expected! Changes: Network P1!";
   }
 }
